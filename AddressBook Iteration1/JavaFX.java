d61dadaf08fe939b1d56cc056893efdd460b1419
wmasters62693 <wmasters.62693@farnborough.ac.uk>
2017-06-27 00:44:52 +0100

    Update JavaFX.java

--- AddressBook Iteration1/JavaFX.java
+++ AddressBook Iteration1/JavaFX.java
@@ -12,12 +12,24 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
 import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import java.util.ArrayList;
+import java.io.*;
+import javafx.scene.paint.Color;
+import java.util.*;
 
 public class JavaFX
 {
+    public static String[][] Contacts;
+    
     public static void LaunchFX()
     {
+        //System.out.println("TEST");
+        
         new JFXPanel();
         
         Platform.runLater(() -> initialiseGUI());
@@ -26,29 +38,178 @@ public class JavaFX
     public static void initialiseGUI()
     {
         Stage stage = new Stage();
-        stage.setTitle("V-1.0.0  ");
+        stage.setTitle("Contact Book  -  V-1.0.0");
         stage.setResizable(false);
         Pane rootPane = new Pane();
-        stage.setScene(new Scene(rootPane));
+        Scene scene = new Scene (rootPane, 1280, 720);
+        scene.getStylesheets().add("CSS.css");
+        stage.setScene(scene);
         stage.setWidth(1280);
         stage.setHeight(720);
         stage.setOnCloseRequest((WindowEvent we) -> Terminate());
         
-        Button contact1Btn = new Button ("contact1Btn");
-        Button contact2Btn = new Button ("contact2Btn");
+        ////Gets peoples addresses from a txt file and saves them to an array
+        try{
+            Contacts = GetContacts();
+        } catch(IOException e) {
+            System.out.println("IOException problem");
+        }
+        
+        ////    Main Page    ////
+        //filling profile picutre
+        Rectangle rPP = new Rectangle();
+        rPP.setX(200 + 100);
+        rPP.setY(50 + 50);
+        rPP.setWidth(150);
+        rPP.setHeight(150);
+        
+        Group mainG = new Group();
+        mainG.getChildren().addAll(rPP);
+        
+        
+        
+        
+        ////    Contact Bar    ////
         VBox contactBox = new VBox();
-        contactBox.setPadding(new Insets(0));
+        contactBox.setPadding(new Insets(50, 0 , 0, 0)); //(top/right/bottom/left)
         contactBox.setSpacing(0);
-        contactBox.setStyle("-fx-background: #FF0000;");
+        contactBox.setStyle("-fx-background-color: #9ea2a3;");
         contactBox.setPrefWidth(200);
+        contactBox.setPrefHeight(720);
+        contactBox.setLayoutX(0);
+        contactBox.setLayoutY(0);
+        //contactBox.getChildren().addAll(contact1Btn, contact2Btn, NewContact);
         
-        contactBox.getChildren().addAll(contact1Btn, contact2Btn);
-        rootPane.getChildren().add(contactBox);
+        
+        ////sets the amount of contact buttons to the Vbox 
+        for (int i = 0; i<Contacts[0].length; i++)
+        {
+            Button btn = new Button(Contacts[0][i]);
+            btn.setId("contactButton");
+            btn.setPrefWidth(200);
+            btn.setPrefHeight(50);
+            final int j = i;
+            btn.setOnAction((ActionEvent ae) -> UpdateGroup(j, mainG));
+            contactBox.getChildren().addAll(btn);
+        }
+        
+        /*Button contact1Btn = new Button ("Person 1");
+        contact1Btn.setId("contactButton");
+        contact1Btn.setPrefWidth(200);
+        contact1Btn.setPrefHeight(50);
+        
+        Button contact2Btn = new Button ("Person 2");
+        contact2Btn.setId("contactButton");
+        contact2Btn.setPrefWidth(200);
+        contact2Btn.setPrefHeight(50);*/
+        
+        
+        Button NewContact = new Button ("New Contact");
+        NewContact.setId("FuncBtn");
+        NewContact.setPrefWidth(200);
+        NewContact.setPrefHeight(50);
+        //NewContact.setLayoutX(1230);
+        NewContact.setAlignment(Pos.BOTTOM_LEFT);
+        contactBox.getChildren().addAll(NewContact);
         
         
         
+        ////   Navigation Bar    ////
+        Button EditBtn = new Button ("Edit");
+        EditBtn.setId("FuncBtn");
+        EditBtn.setPrefWidth(75);
+        EditBtn.setPrefHeight(50);
         
+        HBox NavBar = new HBox();
+        NavBar.setPadding(new Insets(0));
+        NavBar.setStyle("-fx-background-color: #9ea2a3;");
+        NavBar.setPrefWidth(1080);
+        NavBar.setPrefHeight(50);
+        NavBar.setLayoutX(200);
+        NavBar.setLayoutY(0);
+        NavBar.setPadding(new Insets(0, 0 , 0, 999)); //(top/right/bottom/left)
+        NavBar.getChildren().addAll(EditBtn);
+        
+        
+        rootPane.getChildren().addAll(contactBox, NavBar, mainG);
         stage.show();
+        
+    }
+    
+    public static String[][] GetContacts() throws IOException
+    {
+        FileReader fr = new FileReader ("Contacts.txt");
+        BufferedReader br = new BufferedReader(fr);
+        String line = br.readLine();
+        int numOflines =0;
+        do
+        {
+            numOflines++;
+        } while ((line = br.readLine()) != null);
+        System.out.println("number Of lines: " + numOflines);
+        br.close();
+        
+        String[][] Contacts = new String[3][numOflines];
+        fr = new FileReader ("Contacts.txt");
+        br = new BufferedReader(fr);
+        line = br.readLine();
+        
+        int j=0;
+        do
+        {
+            String[] Components = line.split(", ");
+            System.out.println("Component:  " + Components[0] + " " + Components[1] + " " + Components[2]);
+            System.out.println("Components.length: " + Components.length);
+            for (int i = 0; i<Components.length; i++)
+            {
+                Contacts[i][j] = Components[i];
+            }
+            j++;
+        } while ((line = br.readLine()) != null);
+        
+        /*System.out.println("'''");
+        for (int i=0; i<2; i++)
+        {
+            System.out.println(Contacts[0][i] + " " + Contacts[1][i] + " " + Contacts[2][i]);
+        }
+        //System.out.println(i);*/
+        br.close();
+        
+        return (Contacts);
+    }
+    
+    public static void UpdateGroup(int i, Group mainG)
+    {
+        Random rand = new Random();
+        int r = ((int)(rand.nextFloat()*255));
+        int g = ((int)(rand.nextFloat()*255));
+        int b = ((int)(rand.nextFloat()*255));
+        System.out.println(r + " " + g + " " + b); 
+        
+        
+        System.out.println("J = " + i);
+        System.out.println("Contact [0][0]: " + Contacts[0][i]);
+        
+        mainG.getChildren().clear();
+        
+        Label Name = new Label(Contacts[0][i]);
+        Name.setLayoutX(500);
+        Name.setLayoutY(100);
+        Name.setId("Name");
+        
+        Label Address = new Label(Contacts[0][i] + "'s home address: \n\t" + Contacts[1][i]);
+        Address.setLayoutX(500);
+        Address.setLayoutY(150);
+        Address.setId("Address");
+        
+        Rectangle fillInProfilePic = new Rectangle();
+        fillInProfilePic.setX(200 + 100);
+        fillInProfilePic.setY(50 + 50);
+        fillInProfilePic.setWidth(150);
+        fillInProfilePic.setHeight(150);
+        fillInProfilePic.setFill(Color.rgb(r, g, b, 1));
+        //fillInProfilePic.setFill(Color.rgb(0,5,255, 1));
+        mainG.getChildren().addAll(Name, Address, fillInProfilePic);
         
     }
     
